db617d8f7ff4c7a86ab16150d7932e58de6a2b29
shotastage <shota.shimazu@magicalsoft.app>
2024-11-27 10:30:41 +0900

    refactor package builder to use make for building binary and add file copy functions

--- scripts/package-builder/debuilder.go
+++ scripts/package-builder/debuilder.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 func main() {
@@ -11,44 +12,107 @@ func main() {
 	packageName := "quickflow-server"
 	version := "1.0.0"
 	architecture := "amd64"
-	maintainer := "Shota Shimazu <shota.shimazu@magique.app>"
-	description := "High-performance CMS and API platform written in Go"
+
+	// Define paths
+	resourceDir := "resources/debian"
+	debianDir := "debian"
+	binDir := filepath.Join(debianDir, "usr/bin")
+	binSrc := "bin/quickflow"
 
 	// Create package structure
-	os.MkdirAll("debian/DEBIAN", 0755)
-	os.MkdirAll("debian/usr/bin", 0755)
-
-	// Create control file
-	control := fmt.Sprintf(`Package: %s
-Version: %s
-Section: web
-Priority: optional
-Architecture: %s
-Maintainer: %s
-Description: %s
-`, packageName, version, architecture, maintainer, description)
-
-	err := os.WriteFile("debian/DEBIAN/control", []byte(control), 0644)
+	err := os.MkdirAll(binDir, 0755)
 	if err != nil {
-		fmt.Println("Error writing control file:", err)
+		fmt.Println("Error creating package structure:", err)
 		return
 	}
 
-	// Build the binary
-	cmd := exec.Command("go", "build", "-o", "debian/usr/bin/quickflow")
+	// Copy control files
+	err = copyDir(resourceDir, debianDir)
+	if err != nil {
+		fmt.Println("Error copying control files:", err)
+		return
+	}
+
+	// Build the binary using make
+	cmd := exec.Command("make", "build")
 	err = cmd.Run()
 	if err != nil {
 		fmt.Println("Error building binary:", err)
 		return
 	}
 
+	// Copy the built binary to the package structure
+	err = copyFile(binSrc, filepath.Join(binDir, "quickflow"))
+	if err != nil {
+		fmt.Println("Error copying binary:", err)
+		return
+	}
+
+	// Set permissions for the binary
+	err = os.Chmod(filepath.Join(binDir, "quickflow"), 0755)
+	if err != nil {
+		fmt.Println("Error setting permissions for binary:", err)
+		return
+	}
+
 	// Build the deb package
-	cmd = exec.Command("dpkg-deb", "--build", "debian")
+	cmd = exec.Command("dpkg-deb", "--build", debianDir)
 	err = cmd.Run()
 	if err != nil {
 		fmt.Println("Error building deb package:", err)
 		return
 	}
 
+	// Move the deb package to the current directory
+	err = os.Rename(debianDir+".deb", fmt.Sprintf("%s_%s_%s.deb", packageName, version, architecture))
+	if err != nil {
+		fmt.Println("Error renaming deb package:", err)
+		return
+	}
+
+	// Clean up
+	err = os.RemoveAll(debianDir)
+	if err != nil {
+		fmt.Println("Error cleaning up:", err)
+		return
+	}
+
 	fmt.Println("Deb package built successfully")
+}
+
+// copyDir copies a whole directory recursively
+func copyDir(src string, dst string) error {
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(src, path)
+		if err != nil {
+			return err
+		}
+
+		dstPath := filepath.Join(dst, relPath)
+
+		if info.IsDir() {
+			return os.MkdirAll(dstPath, info.Mode())
+		}
+
+		data, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		return os.WriteFile(dstPath, data, info.Mode())
+	})
+}
+
+// copyFile copies a single file from src to dst
+func copyFile(src, dst string) error {
+	data, err := os.ReadFile(src)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(dst, data, 0755)
 }
